52e14b7ccd56631907e4d0a05de892d1b994d436
Jack Gallant <concise_sparsity287626@outlook.com>
2025-08-20 19:13:57 +0100

    added content to include/system_defs.h

---
+++ include/system_defs.h
@@ -0,0 +1,11 @@
+#ifndef SYSTEM_DEFS_H
+#define SYSTEM_DEFS_H
+
+#define MAX_PROC 4096
+#define TIMEOUT_MS 2500
+#define DEFAULT_PORT 8081
+#define LOG_PATH "/var/log/sysmon.log"
+
+typedef struct {}
+
+#endif // !SYSTEM_DEFS_H
